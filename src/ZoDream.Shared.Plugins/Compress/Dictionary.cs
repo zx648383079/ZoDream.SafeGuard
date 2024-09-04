b39bdfd4b9f093454aa4185d493fc95697c41912
ZoDream <648383079@qq.com>
2024-09-04 12:37:14 +0800

    update

--- src/ZoDream.Shared.Plugins/Compress/Dictionary.cs
+++ src/ZoDream.Shared.Plugins/Compress/Dictionary.cs
@@ -35,8 +35,8 @@ namespace ZoDream.Shared.Plugins.Compress
             if (code < 0)
             {
                 _reader.Seek(0, SeekOrigin.Begin);
+                code = _reader.ReadByte();
             }
-             code = _reader.ReadByte();
             return (byte)code;
             //var i = _postion % 2;
             //if (i == 0)
